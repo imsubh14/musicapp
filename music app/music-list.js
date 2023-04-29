e2a9a955839a22a884e91289acb5f92fd28d7bd2
Shubh Gupta <118635326+imsubh14@users.noreply.github.com>
2023-04-29 20:57:19 +0530

    Add files via upload

---
+++ music app/music-list.js
@@ -0,0 +1,40 @@
+let ShubhMusic = [
+    {
+      name: "Gaichite ",
+      artist: "Misha Xramovi",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Guitar Shikda",
+      artist: "Jassi Gill",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Piya re",
+      artist: "Darshan",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Jannat ve",
+      artist: "Darshan ",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Love me like you do",
+      artist: "Ellie Goulding",
+      img: "music-5",
+      src: "music-5"
+    },
+  
+    {
+      name: "Bones",
+      artist: "Imagine Dragons",
+      img: "music-6",
+      src: "music-6"
+    },
+  
+  ];
